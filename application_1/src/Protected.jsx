38fdd4ee9e39d8c990b28ae1bf056a1c731c038e
SomSingh23 <githubsom@gmail.com>
2024-04-24 07:00:15 +0530

    protected content of app1 changed to 3d animation

--- application_1/src/Protected.jsx
+++ application_1/src/Protected.jsx
@@ -1,5 +1,6 @@
-import { Suspense } from "react";
+import { Suspense, useRef, useEffect } from "react";
 import { useLoaderData, Await } from "react-router-dom";
+import * as THREE from "three";
 import Navbar from "./Navbar";
 function Protected() {
   const { value } = useLoaderData();
@@ -19,7 +20,7 @@ function Protected() {
             return (
               <>
                 <Navbar isLogout={true} />
-                <h1>Protected Data will be shown here</h1>
+                <ThreeDAnimation />
               </>
             );
           }}
@@ -28,5 +29,47 @@ function Protected() {
     </>
   );
 }
+
+const ThreeDAnimation = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return () => {
+      container.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return <div ref={containerRef} />;
+};
 
 export default Protected;
